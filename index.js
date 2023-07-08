80306baf3829d8bca520d6608c38d44c169b5b18
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-07-07 18:43:14 -0700

    my first stageing

---
+++ index.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+let li = document.querySelector("li");
+
+btn.addEventListener("click", function () {
+  ul.classList.toggle("hide");
+});
